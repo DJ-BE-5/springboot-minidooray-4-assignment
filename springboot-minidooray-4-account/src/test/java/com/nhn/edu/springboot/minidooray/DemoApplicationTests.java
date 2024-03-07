d2163fe2c233ab1fbaf2c20cbbbcdf25ed47214d
BHwi <bhwi0422@gmail.com>
2024-03-07 20:27:30 +0900

    add: projects

---
+++ springboot-minidooray-4-account/src/test/java/com/nhn/edu/springboot/minidooray/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.edu.springboot.minidooray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
