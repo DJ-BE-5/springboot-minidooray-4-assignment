57f2dec775944b65616f2a44b1eea38c770ac148
keyoung eun Min <roddmsl0208@gmail.com>
2024-03-09 22:55:04 +0900

    add projectaccountcontroller method

--- springboot-minidooray-4-account/src/main/java/com/nhn/edu/springboot/minidooray/repository/ProjectAccountRepository.java
+++ springboot-minidooray-4-account/src/main/java/com/nhn/edu/springboot/minidooray/repository/ProjectAccountRepository.java
@@ -4,5 +4,13 @@ import com.nhn.edu.springboot.minidooray.entity.Account;
 import com.nhn.edu.springboot.minidooray.entity.ProjectAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectAccountRepository extends JpaRepository<ProjectAccount, Long>{
+
+    Optional<ProjectAccount> findByProjectId(Long projectId);
+
+    Optional<ProjectAccount> findByAccountId(Long accountId);
+
+    Optional<ProjectAccount> findByProjectIdAndAccountId(Long projectId, Long accountId);
 }
--- springboot-minidooray-4-account/src/main/java/com/nhn/edu/springboot/minidooray/service/ProjectAccountService.java
+++ springboot-minidooray-4-account/src/main/java/com/nhn/edu/springboot/minidooray/service/ProjectAccountService.java
@@ -4,6 +4,7 @@ package com.nhn.edu.springboot.minidooray.service;
 import com.nhn.edu.springboot.minidooray.entity.ProjectAccount;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectAccountService {
 
