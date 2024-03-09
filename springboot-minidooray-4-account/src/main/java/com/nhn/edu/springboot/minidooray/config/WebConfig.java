7c78db06df57899e6000130a7053d3fb9b700a88
keyoung eun Min <roddmsl0208@gmail.com>
2024-03-09 17:43:32 +0900

    add projectaccount-controller

--- springboot-minidooray-4-account/src/main/java/com/nhn/edu/springboot/minidooray/config/WebConfig.java
+++ springboot-minidooray-4-account/src/main/java/com/nhn/edu/springboot/minidooray/config/WebConfig.java
@@ -1,16 +1,9 @@
 package com.nhn.edu.springboot.minidooray.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
